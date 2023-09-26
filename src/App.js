5ddebb0c34ee308502cd4bcea49c068a50709ace
SinYoonHa <idc06012@naver.com>
2023-09-26 15:14:59 +0900

    #14 React + 백엔드 통합

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Todo from "./Todo";
 import { Container, List, Paper } from "@material-ui/core";
 import AddTodo from "./AddTodo";
+import { call } from "./service/ApiService";
 
 class App extends React.Component {
   constructor(props) {
@@ -9,31 +10,35 @@ class App extends React.Component {
     super(props); //매개변수 props 초기화
     this.state = {
       //item에 item.id item.title item.done 매개변수 이름과 값 할당
-      items: [
-        { id: "todo0", title: "Todo 1", done: true },
-        { id: "todo1", title: "Todo 2", done: false },
-      ],
+      items: [],
     };
   }
   // add 함수 추가
   add = (item) => {
-    const thisItems = this.state.items;
-    item.id = "ID-" + thisItems.length; //key를 위한 id 추가
-    item.done = false;
-    thisItems.push(item);
-    this.setState({ items: thisItems }); //update state
-    console.log("Items:", this.state.items);
-  };
-
-  //delete 함수 추가. (alt + shift + f 포매팅)
-  delete = (item) => {
-    const thisItems = this.state.items;
-    const newItems = thisItems.filter((e) => e.id !== item.id);
-    this.setState({ items: newItems }, () => {
-      //디버깅 콜백
-      console.log("Update Items: ", this.state.items);
+    call("/todo", "POST", item).then((response) => {
+      this.setState({ items: response.data });
     });
   };
+
+  //update함수
+  update = (item) => {
+    call("/todo", "PUT", item).then((response) => {
+      this.setState({ items: response.data });
+    });
+  };
+
+  //delete 함수
+  delete = (item) => {
+    call("/todo", "DELETE", item).then((response) => {
+      this.setState({ items: response.date });
+    });
+  };
+
+  componentDidMount() {
+    call("/todo", "GET", null).then((response) => {
+      this.setState({ items: response.data });
+    });
+  }
 
   render() {
     //todoItems에 this.state.items.length가 0보다 크다면 true이므로 && 뒤에 값을 넘겨줌
@@ -42,7 +47,12 @@ class App extends React.Component {
       <Paper style={{ margin: 16 }}>
         <List>
           {this.state.items.map((item, idx) => (
-            <Todo item={item} key={item.id} delete={this.delete} />
+            <Todo
+              item={item}
+              key={item.id}
+              delete={this.delete}
+              update={this.update}
+            />
           ))}
         </List>
       </Paper>
--- src/Todo.js
+++ src/Todo.js
@@ -14,13 +14,13 @@ class Todo extends React.Component {
     super(props);
     this.state = { item: props.item, readOnly: true }; //매개변수 item의 변수/값을 item에 대입
     this.delete = props.delete;
+    this.update = props.update;
   }
 
   deleteEventHandler = () => {
     this.delete(this.state.item);
   };
   offReadOnlyMode = () => {
-    console.log("Event!", this.state.readOnly);
     this.setState({ readOnly: false }, () => {
       console.log("ReadOnly?", this.state.readOnly);
     });
@@ -36,10 +36,10 @@ class Todo extends React.Component {
     this.setState({ item: thisItem });
   };
   checkboxEventHandler = (e) => {
-    console.log("check box event call");
     const thisItem = this.state.item;
     thisItem.done = thisItem.done ? false : true; // thisItemdone = !thisitem.done
     this.setState({ item: thisItem });
+    this.update(this.state.item);
   };
   render() {
     const item = this.state.item;
