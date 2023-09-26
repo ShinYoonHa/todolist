e17497144a26e04d10dccda90b121d56c3c8cd82
SinYoonHa <idc06012@naver.com>
2023-09-26 11:37:35 +0900

    #11 React로 목록 추가, 수정, 삭제하기

--- src/App.js
+++ src/App.js
@@ -1,47 +1,59 @@
-import React from 'react'
-import Todo from './Todo';
-import { Container, List, Paper } from '@material-ui/core';
-import './App.css';
-import AddTodo from './AddTodo';
+import React from "react";
+import Todo from "./Todo";
+import { Container, List, Paper } from "@material-ui/core";
+import AddTodo from "./AddTodo";
 
-class App extends React.Component{
-  constructor(props) { //매개변수 props 생성자
+class App extends React.Component {
+  constructor(props) {
+    //매개변수 props 생성자
     super(props); //매개변수 props 초기화
-    this.state = { //item에 item.id item.title item.done 매개변수 이름과 값 할당
-      items : [
-        {id: 0, title: "Todo 1", done: true},
-        {id: 1, title: "Todo 2", done: false},
+    this.state = {
+      //item에 item.id item.title item.done 매개변수 이름과 값 할당
+      items: [
+        { id: "todo0", title: "Todo 1", done: true },
+        { id: "todo1", title: "Todo 2", done: false },
       ],
     };
   }
-  //(1) add 함수 추가
+  // add 함수 추가
   add = (item) => {
     const thisItems = this.state.items;
-    item.id = "ID-"+ thisItems.length; //key를 위한 id 추가
+    item.id = "ID-" + thisItems.length; //key를 위한 id 추가
     item.done = false;
     thisItems.push(item);
-    this.setState({items:thisItems}); //update state
+    this.setState({ items: thisItems }); //update state
     console.log("Items:", this.state.items);
-  }
+  };
+
+  //delete 함수 추가. (alt + shift + f 포매팅)
+  delete = (item) => {
+    const thisItems = this.state.items;
+    const newItems = thisItems.filter((e) => e.id !== item.id);
+    this.setState({ items: newItems }, () => {
+      //디버깅 콜백
+      console.log("Update Items: ", this.state.items);
+    });
+  };
 
   render() {
     //todoItems에 this.state.items.length가 0보다 크다면 true이므로 && 뒤에 값을 넘겨줌
     //todoItem = this.state.items.length > 0 ? (<Paper></Paper>):""; 이렇게 해도 같은 결과임
     var todoItems = this.state.items.length > 0 && (
-      <Paper style={{margin:16}}>
+      <Paper style={{ margin: 16 }}>
         <List>
           {this.state.items.map((item, idx) => (
-            <Todo item={item} key={item.id}/>
+            <Todo item={item} key={item.id} delete={this.delete} />
           ))}
         </List>
       </Paper>
     );
-    //(2) add 함수 연결
-    return (  //생성된 컴포넌트 JSX를 리턴한다.
-      <div className='App'>
+
+    //생성된 컴포넌트 JSX를 리턴한다.
+    return (
+      <div className="App">
         <Container maxWidth="md">
-          <AddTodo add={this.add}/>
-          <div className='TodoList'>{todoItems} </div>
+          <AddTodo add={this.add} />
+          <div className="TodoList">{todoItems} </div>
         </Container>
       </div>
     );
